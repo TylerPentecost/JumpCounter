e1f70f999e5c78bb082b4cbdad520119a747cadc
Tyler Pentecost <tyler8806@gmail.com>
2021-07-24 00:49:58 -0500

    Basic Refactor (#7)
    Reorganized the JumpCounter.lua file to be easier to read.

--- JumpCounter/JumpCounter.lua
+++ JumpCounter/JumpCounter.lua
@@ -1,8 +1,8 @@
 -- File: JumpCounter.lua
 -- Name: JumpCounter
--- Author: Aic
+-- Author: Aic, Zolerii
 -- Description: Counts the number of times you jump
--- Version: 1.1
+-- Version: 0.1.0
 
 local UPDATE_PERIOD = 0.5
 local elapsed = 0
@@ -10,61 +10,69 @@ local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
 local dataObj = ldb:NewDataObject("Jump Counter", {type = "data source", text = "0 Jumps"})
 local frame = CreateFrame("frame")
 
--- Write jumps to data broker
-frame:SetScript("OnUpdate", function(self, elap)
-	elapsed = elapsed + elap
-  if elapsed < UPDATE_PERIOD then
-    return
-  end
-
-	elapsed = 0
-	local fps = GetFramerate()
-	dataObj.text = string.format("%d Jumps", JumpCounter)
-end)
-
 -- Write data broker tool tip
 function dataObj:OnTooltipShow()
-  self:AddLine("Total Jumps")
+    self:AddLine("Total Jumps")
 end
 
 function dataObj:OnEnter()
-  GameTooltip:SetOwner(self, "ANCHOR_NONE")
-  GameTooltip:SetPoint("TOPLEFT", self, "BOTTOMLEFT")
-  GameTooltip:ClearLines()
-  dataObj.OnTooltipShow(GameTooltip)
-  GameTooltip:Show()
+    GameTooltip:SetOwner(self, "ANCHOR_NONE")
+    GameTooltip:SetPoint("TOPLEFT", self, "BOTTOMLEFT")
+    GameTooltip:ClearLines()
+    dataObj.OnTooltipShow(GameTooltip)
+    GameTooltip:Show()
 end
 
 function dataObj:OnLeave()
-  GameTooltip:Hide()
+    GameTooltip:Hide()
 end
 
-hooksecurefunc( "JumpOrAscendStart", function()
-  if JumpCounter == nil then
-    JumpCounter = 0
-  else
-    if not IsFalling() then
-      JumpCounter = JumpCounter + 1
+local function CreateMilestoneMessage()
+    local icon = "|TInterface\\Icons\\INV_Gizmo_supersappercharge:32|t"
+    local color = "|cFFDAA520"
+    local message = string.format("Congratulations, It's your %sth Jump!", JumpCounter)
+
+    return string.format("%s %s %s %s", icon, color, message, icon)
+end
+
+local function AtMilestone()
+    return JumpCounter == 10 or JumpCounter == 100 or JumpCounter == 1000 or mod(JumpCounter, 5000) == 0
+end
+
+local function AccounceMilestone()
+    if AtMilestone() then
+        DEFAULT_CHAT_FRAME:AddMessage(CreateMilestoneMessage())
     end
-    if JumpCounter == 10 or JumpCounter == 100 or JumpCounter == 1000 or mod(JumpCounter, 5000) == 0 then
-      -- Formatting taken from JumpsCount addon by boolbazaur https://www.curseforge.com/wow/addons/jumps-count
-      DEFAULT_CHAT_FRAME:AddMessage("|TInterface\\Icons\\INV_Gizmo_supersappercharge:32|t |cFFDAA520 Congratulations, It's your " .. JumpCounter .. "th Jump! |TInterface\\Icons\\INV_Gizmo_supersappercharge:32|t")
+end
+
+local function GetCurrentJumpCount()
+    return JumpCounter or 0
+end
+
+local function PerformJump()
+    JumpCounter = GetCurrentJumpCount() + 1
+    dataObj.text = string.format("%d Jumps", GetCurrentJumpCount())
+    AccounceMilestone()
+end
+
+hooksecurefunc(
+    "JumpOrAscendStart",
+    function()
+        if not IsFalling() and not IsFlying() then
+            PerformJump()
+        end
     end
-  end
-end );
+)
 
 -- slash command
 for i, v in ipairs({"jc", "jumpcounter", "jumpcount"}) do
-  _G["SLASH_JUMPCOUNTER" .. i] = "/" .. v
+    _G["SLASH_JUMPCOUNTER" .. i] = "/" .. v
 end
 
 SlashCmdList.JUMPCOUNTER = function(msg)
-  if not msg or msg == "" then
-    if JumpCounter == nil then
-      JumpCounter = 0
+    if not msg or msg == "" then
+        DEFAULT_CHAT_FRAME:AddMessage(string.format("You have jumped %s times.", GetCurrentJumpCount()))
+    elseif (msg == "clear") then
+        JumpCounter = nil
     end
-    DEFAULT_CHAT_FRAME:AddMessage("You have jumped " .. JumpCounter .. " times.");
-  elseif ( msg == "clear") then
-    JumpCounter = 0
-  end
 end
